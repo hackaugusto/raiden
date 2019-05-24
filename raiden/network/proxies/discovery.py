d3a087ab0348696ea34fb5acb48cd352c0c133b2
Augusto F. Hack <hack.augusto@gmail.com>
2019-05-24 10:43:48 -0300

    discovery: typing

--- raiden/network/proxies/discovery.py
+++ raiden/network/proxies/discovery.py
@@ -4,10 +4,11 @@ from eth_utils import is_binary_address, to_checksum_address, to_normalized_addr
 from raiden.constants import NULL_ADDRESS
 from raiden.exceptions import TransactionThrew, UnknownAddress
 from raiden.network.proxies.utils import compare_contract_versions, log_transaction
-from raiden.network.rpc.client import check_address_has_code
+from raiden.network.rpc.client import JSONRPCClient, check_address_has_code
 from raiden.network.rpc.smartcontract_proxy import ContractProxy
 from raiden.network.rpc.transactions import check_transaction_threw
-from raiden.utils import pex, safe_gas_limit
+from raiden.utils import safe_gas_limit
+from raiden.utils.typing import Address, UDPEndpoint
 from raiden_contracts.constants import (
     CONTRACT_ENDPOINT_REGISTRY,
     GAS_REQUIRED_FOR_ENDPOINT_REGISTER,
@@ -23,7 +24,12 @@ class Discovery:
     endpoints for other ethereum-/raiden-addressess.
     """
 
-    def __init__(self, jsonrpc_client, discovery_address, contract_manager: ContractManager):
+    def __init__(
+        self,
+        jsonrpc_client: JSONRPCClient,
+        discovery_address: Address,
+        contract_manager: ContractManager,
+    ) -> None:
         contract = jsonrpc_client.new_contract(
             contract_manager.get_contract_abi(CONTRACT_ENDPOINT_REGISTRY),
             to_normalized_address(discovery_address),
@@ -48,7 +54,7 @@ class Discovery:
         self.not_found_address = NULL_ADDRESS
         self.proxy = proxy
 
-    def register_endpoint(self, node_address, endpoint):
+    def register_endpoint(self, node_address: Address, endpoint: UDPEndpoint) -> None:
         if node_address != self.client.address:
             raise ValueError("node_address doesnt match this node's address")
 
@@ -68,14 +74,16 @@ class Discovery:
             if receipt_or_none:
                 raise TransactionThrew("Register Endpoint", receipt_or_none)
 
-    def endpoint_by_address(self, node_address_bin):
+    def endpoint_by_address(self, node_address_bin: Address) -> UDPEndpoint:
         node_address_hex = to_checksum_address(node_address_bin)
-        endpoint = self.proxy.contract.functions.findEndpointByAddress(node_address_hex).call()
+        endpoint: str = self.proxy.contract.functions.findEndpointByAddress(
+            node_address_hex
+        ).call()
 
         if endpoint == "":
-            raise UnknownAddress("Unknown address {}".format(pex(node_address_bin)))
+            raise UnknownAddress(f"Unknown address {node_address_hex}")
 
-        return endpoint
+        return UDPEndpoint(endpoint)
 
     def version(self):
         return self.proxy.contract.functions.contract_version().call()
